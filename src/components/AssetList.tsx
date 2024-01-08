d27b9c831aff8b8e4e9aecf485917d1cb2ee2879
godzidcrypto <godzidcrypto@gmail.com>
2024-01-08 23:50:12 +1300

    feat: include 0 balance in filter and handle scoop states

--- src/components/AssetList.tsx
+++ src/components/AssetList.tsx
@@ -230,8 +230,12 @@ const AssetList: React.FC = () => {
       .includes(search.toLowerCase());
     const filterZeroBalance =
       !showZeroBalance ||
-      Number(entry[1].asset?.balance) / 10 ** entry[1].asset.token.decimals ===
-        0;
+      Number(
+        (
+          Number(entry[1].asset?.balance) /
+          10 ** entry[1].asset.token.decimals
+        ).toLocaleString()
+      ) === 0;
     const filterStrict = !showStrict || entry[1].asset.token.strict === true;
 
     return nameSearch && filterZeroBalance && filterStrict;
@@ -403,6 +407,7 @@ const AssetList: React.FC = () => {
                           viewBox="0 0 24 24"
                           fill="none"
                           xmlns="http://www.w3.org/2000/svg"
+                          className="text-bonk-green"
                         >
                           <path
                             d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z"
@@ -416,12 +421,14 @@ const AssetList: React.FC = () => {
                           />
                         </svg>
                       ) : (
+                        // X
                         <svg
                           width="24"
                           height="24"
                           viewBox="0 0 24 24"
                           fill="none"
                           xmlns="http://www.w3.org/2000/svg"
+                          className="text-bonk-red-400"
                         >
                           <path
                             d="M16.3956 7.75734C16.7862 8.14786 16.7862 8.78103 16.3956 9.17155L13.4142 12.153L16.0896 14.8284C16.4802 15.2189 16.4802 15.8521 16.0896 16.2426C15.6991 16.6331 15.0659 16.6331 14.6754 16.2426L12 13.5672L9.32458 16.2426C8.93405 16.6331 8.30089 16.6331 7.91036 16.2426C7.51984 15.8521 7.51984 15.2189 7.91036 14.8284L10.5858 12.153L7.60436 9.17155C7.21383 8.78103 7.21383 8.14786 7.60436 7.75734C7.99488 7.36681 8.62805 7.36681 9.01857 7.75734L12 10.7388L14.9814 7.75734C15.372 7.36681 16.0051 7.36681 16.3956 7.75734Z"
@@ -459,7 +466,12 @@ const AssetList: React.FC = () => {
             </div>
             <button
               onClick={scoop}
-              className="block rounded bg-bonk-yellow px-5 py-3 text-sm text-gray-700 transition hover:bg-bonk-yellow/80 w-full"
+              disabled={state === ApplicationStates.SCOOPED}
+              className={`block rounded bg-bonk-yellow px-5 py-3 text-sm text-gray-700 transition w-full ${
+                state === ApplicationStates.SCOOPED
+                  ? "hover:cursor-not-allowed"
+                  : "hover:bg-bonk-yellow/80"
+              }`}
             >
               Confirm
             </button>
@@ -506,39 +518,41 @@ const AssetList: React.FC = () => {
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-200 relative">
-              {state !== ApplicationStates.LOADED_QUOTES && (
-                <tr>
-                  <td className="table-cell" colSpan={5}>
-                    <div className="text-center font-black uppercase text-lg lg:text-4xl bg-white/70 flex items-center gap-2 min-h-48 h-full w-full justify-center animate-pulse">
-                      Fetching Data...{" "}
-                      <svg
-                        width="72"
-                        height="72"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="animate-spin w-12 h-12 lg:w-auto lg:h-auto"
-                      >
-                        <path
-                          opacity="0.2"
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
-                          d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          d="M12 22C17.5228 22 22 17.5228 22 12H19C19 15.866 15.866 19 12 19V22Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
-                          fill="currentColor"
-                        />
-                      </svg>
-                    </div>
-                  </td>
-                </tr>
-              )}
+              {state !== ApplicationStates.LOADED_QUOTES &&
+                state !== ApplicationStates.SCOOPED &&
+                state !== ApplicationStates.SCOOPING && (
+                  <tr>
+                    <td className="table-cell" colSpan={100}>
+                      <div className="text-center font-black uppercase text-lg lg:text-4xl bg-white/70 flex items-center gap-2 min-h-48 h-full w-full justify-center animate-pulse">
+                        Fetching Data...{" "}
+                        <svg
+                          width="72"
+                          height="72"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="animate-spin w-12 h-12 lg:w-auto lg:h-auto"
+                        >
+                          <path
+                            opacity="0.2"
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M12 22C17.5228 22 22 17.5228 22 12H19C19 15.866 15.866 19 12 19V22Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                      </div>
+                    </td>
+                  </tr>
+                )}
               {state === ApplicationStates.LOADED_QUOTES &&
                 filteredData.length === 0 && (
                   <tr>
@@ -718,16 +732,18 @@ const AssetList: React.FC = () => {
           </div>
           <div
             className={`grid gap-2 bg-white rounded-3xl p-4 ${
-              state !== ApplicationStates.LOADED_QUOTES
-                ? "hover:cursor-not-allowed"
-                : ""
+              state !== ApplicationStates.LOADED_QUOTES &&
+              state !== ApplicationStates.SCOOPED &&
+              state !== ApplicationStates.SCOOPING &&
+              "hover:cursor-not-allowed"
             }`}
           >
             <div
               className={`relative ${
-                state !== ApplicationStates.LOADED_QUOTES
-                  ? "pointer-events-none"
-                  : ""
+                state !== ApplicationStates.LOADED_QUOTES &&
+                state !== ApplicationStates.SCOOPED &&
+                state !== ApplicationStates.SCOOPING &&
+                "pointer-events-none"
               }`}
             >
               <label className="sr-only"> Search </label>
@@ -766,9 +782,10 @@ const AssetList: React.FC = () => {
             </div>
             <div
               className={`space-y-2 ${
-                state !== ApplicationStates.LOADED_QUOTES
-                  ? "pointer-events-none"
-                  : ""
+                state !== ApplicationStates.LOADED_QUOTES &&
+                state !== ApplicationStates.SCOOPED &&
+                state !== ApplicationStates.SCOOPING &&
+                "pointer-events-none"
               }`}
             >
               <details className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
@@ -926,9 +943,10 @@ const AssetList: React.FC = () => {
             </div>
             <div
               className={`flex justify-end w-full ${
-                state !== ApplicationStates.LOADED_QUOTES
-                  ? "pointer-events-none"
-                  : ""
+                state !== ApplicationStates.LOADED_QUOTES &&
+                state !== ApplicationStates.SCOOPED &&
+                state !== ApplicationStates.SCOOPING &&
+                "pointer-events-none"
               }`}
             >
               <div
