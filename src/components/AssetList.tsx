f4e80ba5c24291b8b78c98f31fdbdf373da8caf8
godzidcrypto <godzidcrypto@gmail.com>
2024-01-11 19:52:03 +1300

    feat: added tooltip

--- src/components/AssetList.tsx
+++ src/components/AssetList.tsx
@@ -7,7 +7,7 @@ import {
   TokenBalance,
   loadJupyterApi,
   BONK_TOKEN_MINT,
-  getAssetBurnReturn
+  getAssetBurnReturn,
 } from "../scooper";
 import {
   DefaultApi,
@@ -524,8 +524,39 @@ const AssetList: React.FC = () => {
                 <th className="whitespace-nowrap p-4 font-medium text-gray-900 text-lg text-right">
                   Fee (Bonk)
                 </th>
-                <th className="whitespace-nowrap p-4 font-medium text-gray-900 text-lg text-right">
+                <th className="whitespace-nowrap p-4 font-medium text-gray-900 text-lg text-right flex gap-4 justify-end">
                   Token List
+                  <div className="group relative hover:cursor-help max-w-max">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11 10.9794C11 10.4271 11.4477 9.97937 12 9.97937C12.5523 9.97937 13 10.4271 13 10.9794V16.9794C13 17.5317 12.5523 17.9794 12 17.9794C11.4477 17.9794 11 17.5317 11 16.9794V10.9794Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M12 6.05115C11.4477 6.05115 11 6.49886 11 7.05115C11 7.60343 11.4477 8.05115 12 8.05115C12.5523 8.05115 13 7.60343 13 7.05115C13 6.49886 12.5523 6.05115 12 6.05115Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    <div className="hidden bg-black text-white text-center text-xs rounded-lg py-2 absolute z-10 group-hover:block top-6 px-3 -right-6 w-64 md:w-96 hover:cursor-auto text-wrap">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nisi, accusamus impedit a recusandae architecto voluptatum
+                      nostrum natus, quos nam libero quisquam excepturi sequi
+                      optio adipisci voluptatem, dicta maxime repudiandae
+                      exercitationem.
+                    </div>
+                  </div>
                 </th>
                 {/* <th className="whitespace-nowrap p-4 font-medium text-gray-900 text-lg">
                     Status
@@ -659,10 +690,15 @@ const AssetList: React.FC = () => {
                         : "No quote"}
                     </td>
                     <td className="whitespace-nowrap p-4 text-gray-700 text-right font-mono">
-                      {( Number(burnReturn.lamportsAmount) / LAMPORTS_PER_SOL ).toLocaleString()}
+                      {(
+                        Number(burnReturn.lamportsAmount) / LAMPORTS_PER_SOL
+                      ).toLocaleString()}
                     </td>
                     <td className="whitespace-nowrap p-4 text-gray-700 text-right font-mono">
-                      {( Number(burnReturn.feeAmount) / 10 ** 5 ).toLocaleString()}
+                      {(
+                        Number(burnReturn.feeAmount) /
+                        10 ** 5
+                      ).toLocaleString()}
                     </td>
                     <td className="whitespace-nowrap p-4 text-gray-700 text-right">
                       {entry.asset?.token.strict && <p>Strict</p>}
